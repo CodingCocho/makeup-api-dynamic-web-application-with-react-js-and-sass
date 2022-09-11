4e920748bb06babfbdd65318188c7d544e184c13
CodingCocho <raulujr@gmail.com>
2022-09-10 19:18:27 -0700

    sprint 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import HeroImage from '../images/home-hero-image.jpg';
 import {Footer} from '../components/Footer';
 import {Navbar} from '../components/Navbar';
 import './styles/Home.css';
@@ -10,8 +11,53 @@ export const Home = () =>
         >
             <Navbar />
             <section 
-            className="page">
-                Home page
+            className="page"
+            >
+                <div 
+                className="desktop-flex-container"
+                >
+                    <div 
+                    className="left-flex-container"
+                    >
+                        <p 
+                        className="home-heading-1"
+                        >
+                            BEAUTY'S BIGGEST EVENT
+                        </p>
+                        <p 
+                        className="home-heading-2"
+                        >
+                            21 Days of Beauty
+                        </p>
+                        <p 
+                        className="home-heading-3"
+                        >
+                            It's here! The newest arrivals. The hottest exclusives. 50% off must-haves with Daily Beauty Steals®. Get to it.
+                        </p>
+                        <div 
+                        className="shop-now-container"
+                        >
+                            <span 
+                            className="shop-now"
+                            >
+                                Shop now
+                            </span>
+                            <i 
+                            class="fa-solid fa-arrow-right"
+                            >
+                            </i>
+                        </div>
+                    </div>
+                    <div 
+                    className="right-flex-container"
+                    >
+                        <img 
+                        alt="makeup-model" 
+                        className="desktop-hero-image" 
+                        src={HeroImage}
+                        />
+                    </div>
+                </div>    
             </section>
             <Footer />
         </div>        
